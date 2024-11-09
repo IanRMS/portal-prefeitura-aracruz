9bc71c681b35132cb0679c94dbea2de2ea3e1073
ianribeiroms <ian.ribeiroms@gmail.com>
2024-11-09 18:16:08 -0300

    css adjustments

--- portal-prefeitura-aracruz/src/components/pageContainer/index.tsx
+++ portal-prefeitura-aracruz/src/components/pageContainer/index.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const PageContainer = styled.main`
   width: calc(100vw - 320px);
-  padding: 12px 160px 36px 160px;
+  padding: 12px 160px 72px 160px;
   display: flex;
   flex-direction: column;
   gap: 64px;
@@ -10,13 +10,12 @@ export const PageContainer = styled.main`
 
   @media (max-width: 960px) {
     width: calc(100vw - 80px);
-    padding: 8px 40px;
+    padding: 8px 40px 36px 40px;
     gap: 48px;
   }
 
   @media (max-width: 768px) {
     width: calc(100vw - 40px);
-    padding: 8px 20px;
     gap: 32px;
   }
 `;
